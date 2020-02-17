be7ab743cbc596f47a80bff374a3ae0bdffa2be2
Satyajeet Deshmukh <ee170002042@iiti.ac.in>
2020-02-18 01:26:39 +0530

    Error check  for missing points

--- main.R
+++ main.R
@@ -5,11 +5,9 @@ library(forecast)
 library(stats)
 library(tseries)
 library(readr)
-library(tidyr)
-
 
 #import the full data
-Sub_Division_IMD_2017 <- read_csv("Sub_Division_IMD_2017.csv")
+Sub_Division_IMD_2017 <- read.csv("Sub_Division_IMD_2017.csv")
 
 # West madhyad pradesh begins at row 2085 till 2201
 WMP <- Sub_Division_IMD_2017[2084:2200,3:14]
@@ -17,12 +15,20 @@ WMP <- Sub_Division_IMD_2017[2084:2200,3:14]
 #Convert to matrix
 WMP <- as.matrix(WMP)
 
-#Convert data to a single vector
+#Convert data to a single col vector
 WMP <- t(WMP)
 dim(WMP) <- c(117*12,1)
 
-#convert vector data into time series format
-RainfallTS <- ts(WMP, start=c(1901,1), end = c(2017,12), frequency = 12)
-RainfallTS
-plot(RainfallTS)
+if(sum(is.na(WMP)!=0)){
+    print("Missing data points!")
+} else print("Data imported ready for TS. No missing data points.")
+
+#convert vector data into time series format 1901 - 2017
+RainfallTS <- ts(WMP[1:1404,1], start=c(1901,1), frequency = 12)
+
+#decompose into components
+TSDecompose <- decompose(RainfallTS, type = "multiplicative", filter = NULL)
+#plot(TSDecompose) #plots the components
+
+
 

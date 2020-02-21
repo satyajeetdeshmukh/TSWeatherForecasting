e85590b48087f29d3edeb2ca3b539ba3b640acf0
Satyajeet Deshmukh <ee170002042@iiti.ac.in>
2020-02-21 14:49:38 +0530

    added forecast

--- main.R
+++ main.R
@@ -26,9 +26,26 @@ if(sum(is.na(WMP)!=0)){
 #convert vector data into time series format 1901 - 2017
 RainfallTS <- ts(WMP[1:1404,1], start=c(1901,1), frequency = 12)
 
+#uses linear interpolation to replace missing points if any
+tsclean(RainfallTS, replace.missing = TRUE, lambda = NULL)
+         
 #decompose into components
 TSDecompose <- decompose(RainfallTS, type = "multiplicative", filter = NULL)
-#plot(TSDecompose) #plots the components
+plot(TSDecompose) #plots the components
 
+#splitting the data into train and test sets
+TS_Train <- window(RainfallTS, start=c(1901,1), end=c(1999,12), freq=12)
+TS_Test <- window(RainfallTS, start=c(2000,1), freq=12)
 
+model_autoARIMA = auto.arima(TS_Train)
+model_autoARIMA
+summary(model_autoARIMA)
 
+forecast_autoARIMA <- forecast(model_autoARIMA, (12*18))
+plot(forecast_autoARIMA, type = 'h')
+forecast_autoARIMA
+
+vec = cbind(TS_Test,as.data.frame(forecast_autoARIMA)[,1])
+ts.plot(vec, col=c("blue", "red"), main="Actual vs Predicted (AutoARIMA)")
+
+RMSE <- round(sqrt(sum((vec[,1]-vec[,2])^2)/length(vec[,1])),4)
